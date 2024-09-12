2bd02dcf85bc61a195f2a639ef9a3d6652154940
William Wingate <wtwingate@gmail.com>
2024-09-11 23:28:48 -0400

    Set terminal to raw mode while program is running

--- kilo.c
+++ kilo.c
@@ -1,3 +1,72 @@
-int main() {
-  return 0;
+/*** includes ***/
+
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+/*** data ***/
+
+struct termios orig_termios;
+
+/*** terminal ***/
+
+void die(const char *s)
+{
+	perror(s);
+	exit(1);
+}
+
+void disable_raw_mode()
+{
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1) {
+		die("disable_raw_mode => tcsetattr");
+	}
+}
+
+void enable_raw_mode()
+{
+	if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) {
+		die("enable_raw_mode => tcgetattr");
+	}
+	atexit(disable_raw_mode);
+
+	struct termios raw = orig_termios;
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+	raw.c_cc[VMIN] = 0;
+	raw.c_cc[VTIME] = 1;
+
+	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) {
+		die("enable_raw_mode => tcsetattr");
+	}
+}
+
+/*** init ***/
+
+int main()
+{
+	enable_raw_mode();
+	
+	while (1) {
+		char c = '\0';
+		if (read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
+			die("main => read");
+		}
+		if (iscntrl(c)) {
+			printf("%d\r\n", c);
+		} else {
+			printf("%d ('%c')\r\n", c, c);
+		}
+		
+		if (c == 'q') {
+			break;
+		}
+	}
+
+	return 0;
 }
